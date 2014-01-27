9187f293f68f8c6fbc9cbe9a1a6732b9ebdbf3c8
Wesley Ellis <tahnok@gmail.com>
2014-01-27 09:15:46 -0500

    1.16

--- chapter_1.scm
+++ chapter_1.scm
@@ -95,7 +95,7 @@
   (average guess (/ x guess)))
 
 (define (average x y
-  (/ (+ x y) 2))
+  (/ (+ x y) 2)))
 
 (define (good-enough? guess x)
   (< (abs (- (square guess) x)) 0.001))
@@ -178,3 +178,17 @@
 	((> x y) 0)
 	(else (+ (pascal x (- y 1)) (pascal (- x 1) (- y 1))))))
 
+;1.16 Iterative fast exponentiation
+
+(define (square x) (* x x))
+(define (even? n) (= (remainder n 2) 0))
+
+(define (exp-iter b n a)
+  (cond ((= n 0) a)
+	((even? n) (exp-iter (square b) (/ n 2) a))
+	(else (exp-iter b (- n 1) (* a b)))))
+
+(define (fast-exp b n)
+  (exp-iter b n 1))
+
+;;M-x run scheme; C-x C-e to send current line to schemeo
