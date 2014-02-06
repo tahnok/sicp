c4db05aa3d99cc29e01f485a5a4a3e530d2437a2
Wesley Ellis <tahnok@gmail.com>
2014-02-06 10:34:08 -0500

    1.21

--- chapter_1.scm
+++ chapter_1.scm
@@ -227,3 +227,20 @@
 		  p
 		  q
 		  (- count 1)))))
+
+;1.21
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(smallest-divisor 199) ;value 199
+(smallest-divisor 1999) ;value 1999
+(smallest-divisor 199) ;value 7
