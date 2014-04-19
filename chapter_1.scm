63c11da64be1413c05a076a40026ee7ae9457d7e
Wesley Ellis <tahnok@gmail.com>
2014-04-19 10:05:08 -0400

    1.35 and 1.36

--- chapter_1.scm
+++ chapter_1.scm
@@ -613,3 +613,51 @@
 
 ;it would call (2 2) and fail since you can't call 2
 
+;1.35
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+;p^2 = p + 1
+
+;p = p^2 - 1
+
+;1 = p - 1/p
+
+;p = 1 + 1 / p
+
+(define (golden-ratio)
+  (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1))
+
+;1.36
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (display guess)
+      (newline)
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (x-to-x)
+  (fixed-point (lambda (x) (/ (log 1000) (log x))) 1.1))
+
+(define (x-to-x-average)
+  (define (average x y)
+    (/ (+ x y) 2))
+  (fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) 1.1))
+
+;far fewer iterations for x-to-x-average
