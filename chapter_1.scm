989d31cabaa12da964365104df791e285156efa3
Wesley Ellis <tahnok@gmail.com>
2014-04-30 16:00:21 -0400

    1.45

--- chapter_1.scm
+++ chapter_1.scm
@@ -743,3 +743,35 @@
 (define (smooth f)
   (lambda (x)
     (/ (+ (f x) (f (+ x dx)) (f (- x dx))) 3)))
+
+;1.45
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (repeated f n)
+  (if
+   (= n 1)
+   f
+   (lambda (x)
+     (f ((repeated f (- n 1)) x)))))
+
+(define (log2 n)
+  (/ (log n) (log 2)))
+
+(define (nth-root n radicand)
+  (fixed-point ((repeated average-damp (floor (log2 n))) (lambda (y) (/ n (expt y (- radicand 1))))) 1.0))
+
